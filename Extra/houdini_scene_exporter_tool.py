3e59cbf4f65f17b1e832fa3718579c16a5b2ebf9
Oleg Pustovit <ursus95@gmail.com>
2018-10-10 21:03:35 +0300

    Binary scene exporter finished

--- Extra/houdini_scene_exporter_tool.py
+++ Extra/houdini_scene_exporter_tool.py
@@ -1,24 +1,54 @@
 # shingine scene exporter for houdini
 # *.ssd (shingine scene description)
-# collect all of the object nodes in the obj
+# format structure
+'''
+HEADER 
+{
+    signature U24
+    version U8
+    node_count U16
+    nodes NODE[node_count]
+}
+NODE
+{
+    node_begin U8
+    id U16
+    parent_id U16
+    type U8
+    name U8[32]
+    attribute_count U8
+    attributes ATTRIBUTE[attribute_count]
+    node_end U8
+}
+ATTRIBUTE
+{
+    attr_begin U8
+    name U8[32]
+    container_type U8
+    data_type U8
+    element_count U32
+    value ARBITRARY_DATA[element_count]
+    attr_end U8
+}
+'''
 
-from enum import Enum
+from enum import IntEnum
+import struct
 
-class ContainerType(Enum):
+class ContainerType(IntEnum):
     NODE = 1
     VALUE = 3
 
-class DataType(Enum):
+class DataType(IntEnum):
     NONE = 0
     BYTE = 1
     UINT = 2
-    INT = 3
     FLOAT = 4
 
-class LightType(Enum):
+class LightType(IntEnum):
     POINT = 1
 
-class NodeType:
+class NodeType(IntEnum):
     LIGHT = 0
     TRANSFORM = 1
     MATERIAL = 2
@@ -71,7 +101,7 @@ for id, node in enumerate(hom_nodes):
     if node.type().name() == "light":
         light_node = Node(type=NodeType.LIGHT)
         light_node.attributes.append(Attribute("Color", ContainerType.VALUE, DataType.FLOAT, 3, node.parmTuple("light_color").eval()))
-        light_node.attributes.append(Attribute("Exposure", ContainerType.VALUE, DataType.FLOAT, 1, node.parmTuple("light_exposure").eval()))
+        light_node.attributes.append(Attribute("Exposure", ContainerType.VALUE, DataType.FLOAT, 1, node.parm("light_exposure").eval()))
         light_node.attributes.append(Attribute("Intensity", ContainerType.VALUE, DataType.FLOAT, 1, node.parm("light_intensity").eval()))
         nodes[id].attributes.append(Attribute("Light", ContainerType.NODE, DataType.NONE, 1, light_node))
     
@@ -98,7 +128,6 @@ for id, node in enumerate(hom_nodes):
             positions.append(point.position().y())
             positions.append(point.position().z())
 
-            print point
             normal_value = point.attribValue("N")
             normals.append(normal_value[0])
             normals.append(normal_value[1])
@@ -108,8 +137,6 @@ for id, node in enumerate(hom_nodes):
             texcoord.append(0.0)
             texcoord.append(0.0)
             texcoord.append(0.0)
-            
-            # normals.append(point.attribValue("uv").
 
         for face in geometry.prims():
             for vert in face.vertices():
@@ -134,4 +161,83 @@ for id, node in enumerate(hom_nodes):
                 material_node = Node(type=NodeType.MATERIAL)
                 material_node.attributes.append(Attribute("DiffuseColor", ContainerType.VALUE, DataType.FLOAT, 3, node.parmTuple("basecolor").eval()))
                 nodes[id].attributes.append(Attribute("Material", ContainerType.NODE, DataType.NONE, 1, material_node))
-print "all good"
+
+version = 1
+signature = "SSD"
+node_begin = 0xaa
+node_end = 0xab
+attr_begin = 0xba
+attr_end = 0xbb
+character_limit = 32
+
+def get_uint16_to_bytes(val):
+    val = val & 0xffff
+    return ((val >> 8) & 0xff, val & 0xff)
+
+def get_uint32_to_bytes(val):
+    val = val & 0xffffffff
+    return (
+        (val << 24) & 0xff, 
+        (val << 16) & 0xff, 
+        (val << 8) & 0xff, 
+        val & 0xff)
+
+def string_to_bytes(s):
+    return bytearray(s[:character_limit])
+    
+def attribute_to_bytes(attr):
+    attr_data = bytearray(attr_begin)
+    attr_data.extend(string_to_bytes(attr.name))
+    attr_data.append(int(attr.container_type) & 0xff)
+    attr_data.append(int(attr.data_type) & 0xff)
+    attr_data.extend(get_uint32_to_bytes(attr.element_count))
+
+    values = attr.value
+    if attr.element_count == 1:
+        values = [attr.value,]
+
+    for x in range(len(values)):
+        if attr.container_type == ContainerType.NODE:
+            attr_data.extend(node_to_bytes(values[x]))
+            continue
+        if (attr.data_type == DataType.FLOAT):
+            attr_data.extend(bytearray(struct.pack("f", values[x])))
+        if (attr.data_type == DataType.UINT):
+            data.extend(get_uint16_to_bytes(values[x]))
+        if (attr.data_type == DataType.BYTE):
+            data.extend(values[x] & 0xff)
+
+    attr_data.append(attr_end)
+    return attr_data
+
+def node_to_bytes(node):
+    node_data = bytearray(node_begin)
+    node_data.extend(get_uint16_to_bytes(node.id))
+    node_data.extend(get_uint16_to_bytes(node.parent_id))
+    node_data.append(node.type & 0xff)
+    node_data.extend(string_to_bytes(node.name))
+    node_data.append(len(node.attributes) & 0xff)
+    print node.name
+    print node.attributes
+    for x in range(len(node.attributes)):
+        node_data.extend(attribute_to_bytes(node.attributes[x]))
+    node_data.append(node_end)
+    return node_data
+
+# file select
+out_file = hou.expandString(hou.ui.selectFile(pattern="*.ssd"))
+
+if not out_file.endswith(".ssd"):
+    out_file += ".ssd"
+
+data = bytearray(signature)
+data.append(version)
+data.extend(get_uint16_to_bytes(len(nodes)))
+
+for node in nodes:
+    # write nodes
+    data.extend(node_to_bytes(node))
+    
+f = open(out_file, "w+b")
+f.write(data)
+f.close()

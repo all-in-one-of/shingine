9eff9375383e328b390b72d2d0831175ab647a04
Oleg Pustovit <ursus95@gmail.com>
2018-10-22 14:08:02 +0300

    Add default shader asset at initialization

--- Source/App.cpp
+++ Source/App.cpp
@@ -4,12 +4,34 @@
 #include "Modules/Statics/CComponentManager.h"
 #include "Modules/Statics/CEntityManager.h"
 
+#include "Modules/Graphics/IShader.h"
+#include "Modules/Graphics/CGraphics.h"
+#include "Solver/CSolver.h"
+
+void Initialize()
+{
+    // set default shader
+    IShader* defaultShader = dynamic_cast<IShader*>(
+        CAssetManager::Get()->AddAssetOfType("Shader"));
+    CString vertSrc, fragSrc;
+    CResourceLoader::LoadText("Assets/Shaders/default.vert", vertSrc);
+    CResourceLoader::LoadText("Assets/Shaders/default.frag", fragSrc);
+    defaultShader->AddSource(EShaderType::VERTEX, vertSrc);
+    defaultShader->AddSource(EShaderType::FRAGMENT, fragSrc);
+    CGraphics::SetDefaultShader(defaultShader);
+
+}
+
 int main()
 {
+
     bool didLoad = CResourceLoader::Load("Assets/Scenes/TestScene.ssd");
-    auto assets = CAssetManager::Get();
-    auto components = CComponentManager::Get();
-    auto entities = CEntityManager::Get();
+    CSolver* solver = new CSolver();
+    solver->AddSystem("TransformSystem");
+    solver->AddSystem("RenderingSystem");
+    solver->InitializeSystems();
+    while (solver->Simulate());
+
 
     return 0;
 }

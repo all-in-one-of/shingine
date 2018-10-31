1e507b8771b2cf61a1395be3a73bf5ec786a0645
Oleg Pustovit <ursus95@gmail.com>
2018-10-31 12:51:44 +0200

    Added sky and directional light on initialization.

--- Include/Application/Prototyping.h
+++ Include/Application/Prototyping.h
@@ -1,15 +1,18 @@
 #pragma once
-
 #include "Modules/Statics/IActiveCamera.h"
-#include "Engine/Components/TransformComponent.h"
-#include "Game/FirstPersonController/FirstPersonComponent.h"
-#include "Engine/Components/RendererComponent.h"
 #include "Modules/Statics/IComponentManager.h"
+#include "Modules/Statics/IEntityManager.h"
 
 #include "Engine/AssetTypes/Material.h"
 #include "Modules/Statics/IAssetManager.h"
 #include "Modules/Graphics/GraphicsUtils.h"
 #include "Modules/Graphics/GraphicsUtils.h"
+
+#include "Engine/Components/TransformComponent.h"
+#include "Engine/Components/RendererComponent.h"
+#include "Engine/Components/SkyLightComponent.h"
+#include "Engine/Components/LightComponent.h"
+#include "Game/FirstPersonController/FirstPersonComponent.h"
 
 void AddFirstPersonController()
 {
@@ -28,26 +31,69 @@ void AddFirstPersonController()
 
 void TestStuff()
 {
+    IComponentManager* componentManager = Statics::Get<IComponentManager>();
+
     // load texture
     ISerializedClass *tex;
     ResourceLoader::LoadBitmap("Assets/Textures/uv_checker.bmp", tex);
-
+    // add default shader
     IShader *shader = GraphicsUtils::CreateVertexFragmentShader(
-        "Assets/Shaders/AlbedoDrawer.vert", "Assets/Shaders/AlbedoDrawer.frag");
+        "Assets/Shaders/SimpleLighting.vert", "Assets/Shaders/SimpleLighting.frag");
+    // add material with the lighting 
     Material *mat = Statics::Get<IAssetManager>()->AddAssetOfType<Material>();
     mat->Name = "TexturePreviewMaterial";
     mat->ShaderId = shader->AssetId();
     mat->SetTexture("MainTex", tex->UniqueID());
 
-    IComponentManager::StringMap::iterator iterator;
-    Statics::Get<IComponentManager>()->GetComponentIteratorOfType("RendererComponent", iterator);
-    IComponentManager::IdMap &map = iterator->second;
-    IComponentManager::IdMap::iterator it = map.begin();
-
-    for(it = map.begin(); it != map.end(); it++)
+    // set the same lighting material to the each object on the scene
     {
-        RendererComponent* r = dynamic_cast<RendererComponent*>(it->second);
-        r->MaterialReference = mat->UniqueID();
+        IComponentManager::StringMap::iterator iterator;
+        componentManager->GetComponentIteratorOfType("RendererComponent", iterator);
+        IComponentManager::IdMap &map = iterator->second;
+        IComponentManager::IdMap::iterator it = map.begin();
+
+        for(it = map.begin(); it != map.end(); it++)
+        {
+            RendererComponent* r = dynamic_cast<RendererComponent*>(it->second);
+            r->MaterialReference = mat->UniqueID();
+        }
+    }
+    
+    // add skylight component if there isn't
+    SkyLightComponent* skyLight = componentManager->GetComponentOfType<SkyLightComponent>();
+    if (!skyLight)
+        Statics::Get<IEntityManager>()->CreateEntity(
+            {"TransformComponent", "SkyLightComponent", "ObjectMetadataComponent"});
+
+
+    // Add a directional light if there isn't
+    {
+        IComponentManager::StringMap::iterator iterator;
+        componentManager->GetComponentIteratorOfType("LightComponent", iterator);
+        IComponentManager::IdMap &map = iterator->second;
+        IComponentManager::IdMap::iterator it = map.begin();
+        bool directionalLight = false;
+
+        for(it = map.begin(); it != map.end(); it++)
+        {
+            LightComponent* r = dynamic_cast<LightComponent*>(it->second);
+            directionalLight = r->LightType == 2;
+            if (directionalLight) break;
+        }
+        if (!directionalLight)
+        {
+            unsigned int newEntity = Statics::Get<IEntityManager>()->CreateEntity(
+                {"TransformComponent", "LightComponent", "ObjectMetadataComponent"});
+            LightComponent* light = componentManager->GetComponentOfType<LightComponent>(newEntity);
+
+            light->LightType = 2;
+            light->ShadowEnabled = 1;
+
+            TransformComponent* xform = componentManager->GetComponentOfType<TransformComponent>(newEntity);
+            glm::quat rotation = glm::quat(glm::vec3(glm::radians(50.f), glm::radians(-30.f), glm::radians(0.f)));
+            xform->SetRotation(rotation);
+        }
     }
 
+    
 }
